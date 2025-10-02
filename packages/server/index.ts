17bbac441e0d3c99ff8bf3576e0eea8818b9d362
Matt Allen <allen.matt@gmail.com>
2025-10-02 17:08:16 -0400

    initial commit

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,10 +8,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Test display...');
+  res.send('Hello World!');
 });
 app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({ message: 'Test GET...' });
+  res.json({ message: 'Hello World!' });
 });
 
 app.listen(port, () => {
