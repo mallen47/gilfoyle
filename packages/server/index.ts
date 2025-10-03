2455e84f54c83a9a9a165f36559f4c63c7b99670
Matt Allen <allen.matt@gmail.com>
2025-10-03 09:49:40 -0400

    extract chat controller

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import z from 'zod';
-import { chatService } from './services/chat.service';
+import { chatController } from './controllers/chat.controller';
 
 dotenv.config();
 
@@ -17,31 +16,7 @@ app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
 });
 
-const chatSchema = z.object({
-  prompt: z
-    .string()
-    .trim()
-    .min(1, 'Prompt is required.')
-    .max(1000, 'Max prompt length is 1000 characters.'),
-  conversationId: z.uuid(),
-});
-
-app.post('/api/chat', async (req: Request, res: Response) => {
-  const parseResult = chatSchema.safeParse(req.body);
-  if (!parseResult.success) {
-    res.status(400).json({ errors: parseResult.error.issues });
-    return;
-  }
-
-  try {
-    const { prompt, conversationId } = req.body;
-    const response = await chatService.sendMessage(prompt, conversationId);
-
-    res.json({ message: response.message });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to generate a response.' });
-  }
-});
+app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
