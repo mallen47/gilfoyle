5fa10681af02cb69617283f311796bf4401d6ad8
Matt Allen <allen.matt@gmail.com>
2025-10-15 07:54:31 -0400

    display summarize button

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import Skeleton from 'react-loading-skeleton';
+import { HiSparkles } from 'react-icons/hi2';
 import StarRating from './StarRating';
 import { useQuery } from '@tanstack/react-query';
+import { Button } from '../ui/button';
 
 type Props = {
   productId: number;
@@ -54,17 +56,33 @@ const ReviewList = ({ productId }: Props) => {
     return <p className="text-red-500">Unable to fetch reviews. Try again!</p>;
   }
 
+  if (!reviewData?.reviews.length) {
+    return null;
+  }
+
   return (
-    <div className="flex flex-col gap-5">
-      {reviewData?.reviews.map((review) => (
-        <div key={review.id}>
-          <div className="font-semibold">{review.author}</div>
-          <div>
-            <StarRating value={review.rating} />
+    <div>
+      <div className="mb-5">
+        {reviewData?.summary ? (
+          <p>{reviewData.summary}</p>
+        ) : (
+          <Button>
+            <HiSparkles />
+            Summarize
+          </Button>
+        )}
+      </div>
+      <div className="flex flex-col gap-5">
+        {reviewData?.reviews.map((review) => (
+          <div key={review.id}>
+            <div className="font-semibold">{review.author}</div>
+            <div>
+              <StarRating value={review.rating} />
+            </div>
+            <p className="py-2">{review.content}</p>
           </div>
-          <p className="py-2">{review.content}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
