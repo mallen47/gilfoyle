96fc5155f6716aeaa7a297c247a6a85d82fdc9af
Matt Allen <allen.matt@gmail.com>
2025-10-15 08:26:09 -0400

    improve UX for displaying skeletons when summarizing

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import Skeleton from 'react-loading-skeleton';
 import { HiSparkles } from 'react-icons/hi2';
 import StarRating from './StarRating';
 import { useQuery } from '@tanstack/react-query';
 import { Button } from '../ui/button';
 import { useState } from 'react';
+import ReviewSkeleton from './ReviewSkeleton';
 
 type Props = {
   productId: number;
@@ -29,6 +29,7 @@ type SummarizeResponse = {
 
 const ReviewList = ({ productId }: Props) => {
   const [summary, setSummary] = useState('');
+  const [isSummaryLoading, setIsSummaryLoading] = useState(false);
 
   const {
     data: reviewData,
@@ -40,10 +41,14 @@ const ReviewList = ({ productId }: Props) => {
   });
 
   const handleSummarize = async () => {
+    setIsSummaryLoading(true);
+
     const { data } = await axios.post<SummarizeResponse>(
       `/api/products/${productId}/reviews/summarize`
     );
+
     setSummary(data.summary);
+    setIsSummaryLoading(false);
   };
 
   const fetchReviews = async () => {
@@ -58,9 +63,7 @@ const ReviewList = ({ productId }: Props) => {
       <div className="flex flex-col gap-5">
         {[1, 2, 3, 4, 5].map((i) => (
           <div key={i}>
-            <Skeleton width={150} />
-            <Skeleton width={100} />
-            <Skeleton count={2} />
+            <ReviewSkeleton key={i} />
           </div>
         ))}
       </div>
@@ -82,10 +85,21 @@ const ReviewList = ({ productId }: Props) => {
         {currentSummary ? (
           <p>{currentSummary}</p>
         ) : (
-          <Button onClick={handleSummarize}>
-            <HiSparkles />
-            Summarize
-          </Button>
+          <div>
+            <Button
+              onClick={handleSummarize}
+              className="cursor-pointer"
+              disabled={isSummaryLoading}
+            >
+              <HiSparkles />
+              Summarize
+            </Button>
+            {isSummaryLoading && (
+              <div className="py-3">
+                <ReviewSkeleton />
+              </div>
+            )}
+          </div>
         )}
       </div>
       <div className="flex flex-col gap-5">
